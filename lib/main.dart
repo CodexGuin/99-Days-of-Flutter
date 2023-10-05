06fb8c90cf0c4ab85b2443fbd30f6c087ca16dfc
Darren Seah <darrenseahyongliang@protonmail.com>
2023-10-05 17:27:18 +0800

    Day 2 - Theme provider
    Working theme switcher. Simpler day.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:ninety_nine_days_of_flutter/day_2/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 // ! Import the main file for the day you want to run.
-import 'day_1/day_1_main.dart';
-//import 'day_2/day_2_main.dart';
+//import 'day_1/day_1_main.dart';
+import 'day_2/day_2_main.dart';
 
 // * Main function after selecting day_x_main file
 void main() {
-  runApp(const MainApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MainApp(),
+  ));
 }
